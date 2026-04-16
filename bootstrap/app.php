e3c79e62050224bd247dfd3847dacbcb61e70115
Miguel Alejandro Núñez Dávila <mn513819@gmail.com>
2026-04-16 00:25:03 -0600

    Enable HandleCors middleware in Laravel 11 bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,6 +10,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
         $middleware->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions) {
