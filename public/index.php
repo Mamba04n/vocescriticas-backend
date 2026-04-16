e81417705c6eab17373e1bd9021f0fd089268f9b
Miguel Alejandro Núñez Dávila <mn513819@gmail.com>
2026-04-16 00:41:52 -0600

    Guarantee Vercel CORS headers early in public index

--- public/index.php
+++ public/index.php
@@ -4,6 +4,22 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+$allowedOrigin = rtrim((string) (getenv('FRONTEND_URL') ?: 'https://frontend-nu-nine-65.vercel.app'), '/');
+$origin = rtrim((string) ($_SERVER['HTTP_ORIGIN'] ?? ''), '/');
+
+if ($origin !== '' && $origin === $allowedOrigin) {
+    header('Access-Control-Allow-Origin: '.$allowedOrigin);
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    header('Access-Control-Allow-Headers: Accept, Authorization, Content-Type, Origin, X-Requested-With, X-CSRF-TOKEN, X-XSRF-TOKEN');
+    header('Vary: Origin');
+}
+
+if (($_SERVER['REQUEST_METHOD'] ?? '') === 'OPTIONS') {
+    http_response_code(204);
+    exit;
+}
+
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
